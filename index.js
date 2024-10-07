74f28f9be1434378afabbd2c78ae17077ebee6ef
Callegari Simona <csimona@cubbli22-gold.local>
2024-10-07 17:37:04 +0300

    changes on index.js

--- index.js
+++ index.js
@@ -104,6 +104,8 @@ let persons = [
     response.json(persons)
   })
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+  
+  const PORT = process.env.PORT || 3001
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
